574f95b7b0d927a016eaa9d26904decc6ba36382
schalox <user@fake>
2017-04-07 18:33:59 +0300

    Create a simple version of the game
    - add a character which can be moved with a keyboard
    - add a simple map
    - make the camera follow the player

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+    private GameObject player;
+
+    public float Speed = 0.1f;
+	
+    void Start()
+    {
+        // player is the parent object of this script
+        player = this.gameObject;
+    }
+    
+	void Update () {
+        // get keyboard or touch screen input
+        float userInputX = Input.GetAxis("Horizontal");
+        float userInputY = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(userInputX * Speed, userInputY * Speed);
+
+        // move the player according to the user input
+        player.transform.Translate(movement);
+    }
+}
